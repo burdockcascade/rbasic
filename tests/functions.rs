0cfcea4eb4eedef38d794ac678dc879d576b9818
Peter Richmond <3448039+burdockcascade@users.noreply.github.com>
2025-02-18 01:20:09 +0000

    ci: more tests

---
+++ tests/functions.rs
@@ -0,0 +1,83 @@
+use rbasic::evaluate;
+
+#[test]
+fn test_simple_function() {
+    let script = r#"
+        function add(a, b)
+            return a + b
+        end
+        return add(1, 2)
+    "#;
+    let result = evaluate(script, None);
+
+    let expected = 3;
+    let value: i64 = result.unwrap().into();
+    assert_eq!(value, expected);
+}
+
+#[test]
+fn test_simple_function_with_variable() {
+    let script = r#"
+        var a = 1;
+        var b = 2;
+        function add(a, b)
+            return a + b
+        end
+        return add(a, b)
+    "#;
+    let result = evaluate(script, None);
+
+    let expected = 3;
+    let value: i64 = result.unwrap().into();
+    assert_eq!(value, expected);
+}
+
+#[test]
+fn test_simple_function_with_expression_as_argument() {
+    let script = r#"
+        function add(a, b)
+            return a + b
+        end
+        return add(1, 2 * 3)
+    "#;
+    let result = evaluate(script, None);
+
+    let expected = 7;
+    let value: i64 = result.unwrap().into();
+    assert_eq!(value, expected);
+}
+
+#[test]
+fn test_simple_function_with_expression_as_argument_and_variable() {
+    let script = r#"
+        var a = 1;
+        var b = 2;
+        function add(a, b)
+            return a + b
+        end
+        return add(a, b * 3)
+    "#;
+    let result = evaluate(script, None);
+
+    let expected = 7;
+    let value: i64 = result.unwrap().into();
+    assert_eq!(value, expected);
+}
+
+#[test]
+fn test_simple_function_with_expression_as_argument_and_variable_and_return() {
+    let script = r#"
+        var a = 1;
+        var b = 2;
+        function add(a, b)
+            return a + b
+        end
+        var result = add(a, b * 3);
+        return result
+    "#;
+    let result = evaluate(script, None);
+
+    let expected = 7;
+    let value: i64 = result.unwrap().into();
+    assert_eq!(value, expected);
+}
